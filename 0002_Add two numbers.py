4409252bdffc3a11f25517f9887d0fcd7add5a8a
Yashas GR <yashasgr306@gmail.com>
2026-06-19 20:36:01 +0530

    Implement function to add two numbers represented by linked lists

---
+++ 0002_Add two numbers.py
@@ -0,0 +1,24 @@
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+
+            total = x + y + carry
+            carry = total // 10
+
+            current.next = ListNode(total % 10)
+
+            current = current.next
+
+            if l1:
+                l1 = l1.next
+
+            if l2:
+                l2 = l2.next
+
+        return dummy.next
