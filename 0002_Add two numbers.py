76cd8de4fbb88b723696f0c34ca1babc4711df11
Yashas GR <yashasgr306@gmail.com>
2026-06-21 20:16:07 +0530

    Refactor addition logic in 0002_Add two numbers.py
    Refactor while loop to handle addition of two numbers.

--- 0002_Add two numbers.py
+++ 0002_Add two numbers.py
@@ -4,10 +4,7 @@ class Solution:
         current = dummy
         carry = 0
 
-        while l1 or l2 or carry:
-            x = l1.val if l1 else 0
-            y = l2.val if l2 else 0
-
+       
             total = x + y + carry
             carry = total // 10
 
