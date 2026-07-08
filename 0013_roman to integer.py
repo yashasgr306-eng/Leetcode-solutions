74c29092ea249a6781ca3a0d4d46cff4237ff195
Yashas GR <yashasgr306@gmail.com>
2026-07-08 19:10:41 +0530

    Update 0013_roman to integer.py

--- 0013_roman to integer.py
+++ 0013_roman to integer.py
@@ -11,11 +11,3 @@ class Solution:
         }
 
         total = 0
-
-        for i in range(len(s)):
-            if i < len(s) - 1 and roman[s[i]] < roman[s[i + 1]]:
-                total -= roman[s[i]]
-            else:
-                total += roman[s[i]]
-
-        return total
