8ae080993507373debd1991e622bfa53a07ffc6a
Yashas GR <yashasgr306@gmail.com>
2026-07-09 18:56:21 +0530

    Refactor longest common prefix logic
    Refactor the longest common prefix function to simplify logic.

--- 0014_longest common prefix.py
+++ 0014_longest common prefix.py
@@ -5,10 +5,4 @@ class Solution:
 
         prefix = strs[0]
 
-        for s in strs[1:]:
-            while not s.startswith(prefix):
-                prefix = prefix[:-1]
-                if prefix == "":
-                    return ""
-
-        return prefix
+     
