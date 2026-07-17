39f2fb35d1c7b9a375ac6ac78f8d285d095aedc7
Yashas GR <yashasgr306@gmail.com>
2026-07-17 19:48:49 +0530

    Implement valid parentheses checker

---
+++ 0029_Valid parentheses.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        stack = []
+
+        pairs = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for ch in s:
+
+            if ch in "({[":
+                stack.append(ch)
+
+            else:
+                if not stack:
+                    return False
+
+                top = stack.pop()
+
+                if top != pairs[ch]:
+                    return False
+
+        return len(stack) == 0
