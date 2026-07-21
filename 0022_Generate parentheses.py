246d5cec30f596f0e2a14a75bc755d8ff85c663d
Yashas GR <yashasgr306@gmail.com>
2026-07-21 18:50:40 +0530

    Create 0022_Generate parentheses.py

---
+++ 0022_Generate parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generateParenthesis(self, n: int):
+        result = []
+
+        def backtrack(current, open_count, close_count):
+            # If the string is complete
+            if len(current) == 2 * n:
+                result.append(current)
+                return
+
+            # Add '(' if we still have some left
+            if open_count < n:
+                backtrack(current + "(", open_count + 1, close_count)
+
+            # Add ')' only if it won't make the string invalid
+            if close_count < open_count:
+                backtrack(current + ")", open_count, close_count + 1)
+
+        backtrack("", 0, 0)
+        return result
