af04d67298c3b42f1eaf9f85162286277627b45a
Yashas GR <yashasgr306@gmail.com>
2026-07-23 20:44:33 +0530

    Update 0022_Generate parentheses.py

--- 0022_Generate parentheses.py
+++ 0022_Generate parentheses.py
@@ -15,6 +15,3 @@ class Solution:
             # Add ')' only if it won't make the string invalid
             if close_count < open_count:
                 backtrack(current + ")", open_count, close_count + 1)
-
-        backtrack("", 0, 0)
-        return result
